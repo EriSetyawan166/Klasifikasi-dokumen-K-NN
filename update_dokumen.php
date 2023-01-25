f009c5c07c43cea622d2c35140bcdf5bb1951225
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2023-01-25 08:27:38 +0700

    removing trash

--- update_dokumen.php
+++ update_dokumen.php
@@ -15,14 +15,4 @@ if ($hasil) {
     // jika gagal, tampilkan pesan error
     echo "Gagal mengupdate data: " . mysqli_error($conn);
 }
-//   $query = "INSERT INTO dokumen (teks, klasifikasi_id) VALUES ('$teks', '$klasifikasi_id')";
-
-//   if (mysqli_query($conn, $query)) {
-//     echo "<script>alert('Data berhasil ditambahkan'); window.location='form-testing.php'</script>";
-//   } else {
-//     echo "Error: " . $query . "<br>" . mysqli_error($conn);
-//   }
-
-
-// mysqli_close($conn);
 ?>
